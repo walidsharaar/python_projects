1bd81aa02c0fd9b849f6c216a3f2ec53091cde01
Sharaar <walidsharar@gmail.com>
2026-03-24 19:51:52 +0100

    add initial project description for Tip Calculator

---
+++ 002_Tip Calculator/main.py
@@ -0,0 +1,11 @@
+'''
+Day 2 Project: Tip Calculator
+This project will use EVERY concept I learned today. 
+
+The Problem:
+I'm at a restaurant. I want to:
+Calculate the tip based on percentage
+Split the bill among multiple people
+Show the final amount each person pays
+'''
+
