29a212ac22e4372cb6d73efe666e72e59efae364
Sharaar <walidsharar@gmail.com>
2026-03-25 00:23:30 +0100

    Add initial implementation for Password Generator

---
+++ 005_Password Generator/main.py
@@ -0,0 +1,25 @@
+# Day 5: Professional Password Generator
+# Concepts: For Loops, Range(), Lists, and Randomization.
+
+
+
+# Day 5: Character Lists (Data Sources)
+
+# Step 1: Capture Parameters (Business Requirements)
+
+
+# Step 2: Initialize an empty list to store the 'Batch' of characters
+# Using a list instead of a string allows for easy shuffling later
+
+
+# Step 3: Use For Loops with the Range() function to populate the list
+# This simulates 'Batch Processing' logic.
+
+
+# Step 4: Randomization (Shuffling the batch)
+
+
+# Step 5: Final String Assembly
+
+
+# Output Results 
