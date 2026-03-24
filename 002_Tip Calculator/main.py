bd35eadbe6b89a6c00edcdaba9069eaeab5b7e95
Sharaar <walidsharar@gmail.com>
2026-03-24 19:52:52 +0100

    add requirements section to Tip Calculator project

--- 002_Tip Calculator/main.py
+++ 002_Tip Calculator/main.py
@@ -9,3 +9,13 @@ Split the bill among multiple people
 Show the final amount each person pays
 '''
 
+'''
+Requirements:
+My program should:
+Ask for the total bill amount
+Ask for the tip percentage (10%, 12%, 15%, or custom)
+Ask how many people are splitting the bill
+Calculate: (bill + tip) / people
+Display the final amount per person (rounded to 2 decimals)
+'''
+
