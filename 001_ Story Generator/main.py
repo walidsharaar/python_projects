b43a55fcbe9a3b6e0f5f663a994704932df2dca6
Sharaar <walidsharar@gmail.com>
2026-03-24 19:23:27 +0100

    to dos of the challenge

---
+++ 001_ Story Generator/main.py
@@ -0,0 +1,7 @@
+# story_generator.py
+"""Main function to run the story generator"""
+    # Get user inputs
+    # Create the story
+    # You'll write the story here using your variables
+    # Print the story
+    # Print the story with formatting
