3e32379adc6984cf0314766c3bf82ddbf4fcaecf
Sharaar <walidsharar@gmail.com>
2026-03-24 19:28:38 +0100

    Day 1 code is completed

--- 001_ Story Generator/main.py
+++ 001_ Story Generator/main.py
@@ -1,7 +1,22 @@
-# story_generator.py
-"""Main function to run the story generator"""
-    # Get user inputs
-    # Create the story
-    # You'll write the story here using your variables
-    # Print the story
-    # Print the story with formatting
+# story_generator.py - CORRECT VERSION
+
+print("Welcome to the Story Generator!")
+print("=" * 40)
+
+# Get user inputs
+name = input("Enter a name: ")
+adjective = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb ending in -ing: ")
+place = input("Enter a place: ")
+
+# Create the story
+# You'll write the story here using your variables
+# Print the story
+print("\nHere's your story:")
+print("=" * 40)    
+# Print the story with formatting
+print(f"Once upon a time, {name} went to {place}. ")
+print(f"They saw a {adjective} {noun} that was {verb}!")
+print(f"{name} couldn't believe their eyes. ")
+print(f'"This is the most {adjective} day ever!" they shouted.')
