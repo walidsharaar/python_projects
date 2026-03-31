81df5e9106f05df5a0287290eb6550836c09026f
Sharaar <walidsharar@gmail.com>
2026-03-31 17:20:56 +0200

    Initialize Arcade Pong Game: set up initial structure and learning goals for advanced OOP and real-time physics

---
+++ 22_Arcade Pong Game/main.py
@@ -0,0 +1,13 @@
+# Day 22: Arcade Pong Game (Advanced OOP & Real-Time Physics)
+# Learning Goals: Multiple Object Interaction, Coordinate Reflection, and Velocity Scaling.
+
+
+#  CONSTANTS =
+
+
+# THE PADDLE MODEL 
+#THE BALL MODEL
+
+#  THE SCOREBOARD 
+
+#  GAME ENGINE 
