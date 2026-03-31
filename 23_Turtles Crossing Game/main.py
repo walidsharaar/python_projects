11daff2078fdcfa6fe6f0fc5658e7ee4dfdea122
Sharaar <walidsharar@gmail.com>
2026-03-31 18:26:29 +0200

    Initialize Turtle Crossing Game: set up initial structure and learning goals for modular OOP and collision detection

---
+++ 23_Turtles Crossing Game/main.py
@@ -0,0 +1,18 @@
+# Day 23: Turtle Crossing Capstone
+# Learning Goals: Modular OOP, Collision Detection, and Resource Management.
+
+
+
+# --- CONSTANTS ---
+
+
+# --- CLASS 1: PLAYER ---
+
+
+# --- CLASS 2: CAR MANAGER ---
+
+
+# --- CLASS 3: SCOREBOARD ---
+
+# --- THE GAME ENGINE ---
+
